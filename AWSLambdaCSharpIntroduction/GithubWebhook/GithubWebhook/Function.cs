590e915b64681c77b3cd29976735691747de9add
guitarrapc <cherishacquire@outlook.com>
2016-12-09 04:28:29 +0900

    Github - SNS - Lambda - Slack Sample

---
+++ AWSLambdaCSharpIntroduction/GithubWebhook/GithubWebhook/Function.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Amazon.Lambda.Core;
+using Amazon.Lambda.Serialization;
+using Newtonsoft.Json;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializerAttribute(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace GithubWebhook
+{
+    public class Function
+    {
+        private readonly string webhookUrl = Environment.GetEnvironmentVariable("SlackWebhookUrl");
+
+        /// <summary>
+        /// A simple function that takes a string and does a ToUpper
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public async Task<Response> FunctionHandlerAsync(SnsRequest input, ILambdaContext context)
+        {
+            var snsMessage = input.Records.FirstOrDefault()?.Sns.Message;
+            if (snsMessage == null)
+            {
+                throw new NullReferenceException(nameof(snsMessage));
+            }
+
+            var githubEvent = input.Records.FirstOrDefault()?.Sns.MessageAttributes.XGithubEvent.Value;
+            if (string.IsNullOrEmpty(githubEvent))
+            {
+                throw new NullReferenceException(nameof(githubEvent));
+            }
+
+            dynamic githubWebhook = JsonConvert.DeserializeObject(input.Records.First().Sns.Message);
+
+            context.Logger.LogLine($"GitHub WebHook triggered!, {githubWebhook}");
+            var message = $@"New GitHub comment posted by {githubWebhook.comment.user.login} at {githubWebhook.repository.name},
+        Url : {githubWebhook.comment.url}
+        Tite : {githubWebhook.issue.title}
+        -----
+        {githubWebhook.comment.body}";
+
+            var payload = new
+            {
+                channel = "#github",
+                username = "AWS Lambda Bot",
+                text = message,
+                icon_url = "https://d0.awsstatic.com/events/new-reinvent/reinvent_launch-page_illustration_lambda.png",
+            };
+
+            var jsonString = JsonConvert.SerializeObject(payload);
+            using (var client = new HttpClient())
+            {
+                var res = await client.PostAsync(webhookUrl, new StringContent(jsonString, Encoding.UTF8, "application/json"));
+                return new Response(res.StatusCode, $"VSTS Build message. Message : {message}");
+            }
+        }
+    }
+
+    public class Response
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public string Body { get; set; }
+
+        public Response(HttpStatusCode code, string body)
+        {
+            StatusCode = code;
+            Body = body;
+        }
+    }
+}
---
+++ AWSLambdaCSharpIntroduction/GithubWebhook/GithubWebhook/SnsRequest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace GithubWebhook
+{
+    public class SnsRequest
+    {
+        public Record[] Records { get; set; }
+    }
+
+    public class Record
+    {
+        public string EventVersion { get; set; }
+        public string EventSubscriptionArn { get; set; }
+        public string EventSource { get; set; }
+        public Sns Sns { get; set; }
+    }
+
+    public class Sns
+    {
+        public string SignatureVersion { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string Signature { get; set; }
+        public string SigningCertUrl { get; set; }
+        public string MessageId { get; set; }
+        public string Message { get; set; }
+        public Messageattributes MessageAttributes { get; set; }
+        public string Type { get; set; }
+        public string UnsubscribeUrl { get; set; }
+        public string TopicArn { get; set; }
+        public string Subject { get; set; }
+    }
+
+    public class Messageattributes
+    {
+        [JsonProperty("X-Github-Event")]
+        public XGithubEvent XGithubEvent { get; set; }
+    }
+
+    public class XGithubEvent
+    {
+        public string Type { get; set; }
+        public string Value { get; set; }
+    }
+}
